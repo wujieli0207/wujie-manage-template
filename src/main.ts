4caf3365412b2d1362980a75a7d97229e6cf045b
wujieli0207 <lwj0207@foxmail.com>
2021-11-14 17:00:27 +0800

    build: 引入 element-plus 并设置按需引入

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,5 @@ import router from "/@/router";
 import "/@/styles/index.scss";
 
 const app = createApp(App);
-app.use(router);
 
 app.mount("#app");
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,8 @@
 import { UserConfigExport, ConfigEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
+// import styleImport from "vite-plugin-style-import";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 
 import { resolve } from "path";
 
@@ -13,7 +16,28 @@ const alias: Record<string, string> = {
 
 export default ({ command, mode }: ConfigEnv): UserConfigExport => {
   return {
-    plugins: [vue()],
+    plugins: [
+      vue(),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+      // styleImport({
+      //   libs: [
+      //     // 按需加载 element-plus
+      //     {
+      //       libraryName: "element-plus",
+      //       esModule: true,
+      //       ensureStyleFile: true,
+      //       resolveStyle: (name: string) => {
+      //         return `element-plus/lib/theme-chalk/${name}.css`;
+      //       },
+      //       resolveComponent: (name: string) => {
+      //         return `element-plus/lib/${name}`;
+      //       },
+      //     },
+      //   ],
+      // }),
+    ],
     resolve: {
       alias,
     },
