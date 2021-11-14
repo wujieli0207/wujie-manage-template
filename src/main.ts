6009720ec7237f1a3c0158a76f973c14123fbe71
wujieli0207 <lwj0207@foxmail.com>
2021-11-14 16:36:25 +0800

    build: 引入 vue-router

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import router from "/@/router";
 
 // 引入自定义样式
 import "/@/styles/index.scss";
 
 const app = createApp(App);
+app.use(router);
 
 app.mount("#app");
