cf7ed8b1fb846ffec1aca456cf3a391264ca99e5
wujieli0207 <lwj0207@foxmail.com>
2021-11-14 16:24:04 +0800

    build: vite 引入自定义别名

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
 import { UserConfigExport, ConfigEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 
+import { resolve } from "path";
+
+const pathResolve = (dir: string): any => {
+  return resolve(__dirname, ".", dir);
+};
+
+const alias: Record<string, string> = {
+  "/@": pathResolve("src"),
+};
+
 export default ({ command, mode }: ConfigEnv): UserConfigExport => {
   return {
     plugins: [vue()],
+    resolve: {
+      alias,
+    },
   };
 };
