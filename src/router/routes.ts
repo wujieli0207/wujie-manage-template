2850cb8592befe77674d0d0736de47985527dd38
wujieli0207 <lwj0207@foxmail.com>
2021-12-08 08:14:21 +0800

    feat：增加表单演示 demo

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -2,11 +2,19 @@ import { RouteRecordRaw } from "vue-router";
 
 const routes: RouteRecordRaw[] = [
   {
-    path: "/",
+    path: "/basicTableDemo",
     name: "basicTableDemo",
     component: () => import("/@/views/demo/table/basicTableDemo/index.vue"),
     meta: {
       title: "基本表格演示demo",
+    },
+  },
+  {
+    path: "/WebFormDemo",
+    name: "WebFormDemo",
+    component: () => import("/@/views/demo/form/WebFormDemo/index.vue"),
+    meta: {
+      title: "Web 端表单演示demo",
     },
   },
 ];
---
+++ src/views/demo/form/WebFormDemo/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <web-form :form-field="formField" />
+</template>
+
+<script lang="ts">
+  import { defineComponent } from "vue";
+  import WebForm from "/@/components/WebForm/index.vue";
+  import type { IWebFormField } from "/@/components/WebForm/type";
+  export default defineComponent({
+    name: "WebFormDemo",
+    components: {
+      WebForm,
+    },
+    setup: (props, context) => {
+      const formField: IWebFormField[] = [
+        {
+          value: "name",
+          label: "姓名",
+          type: "input",
+          require: true,
+          other: {
+            style: "width: 220px",
+          },
+        },
+      ];
+
+      return {
+        formField,
+      };
+    },
+  });
+</script>
