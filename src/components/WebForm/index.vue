18ca395b0c56257829d8a20dbfa795d393016eae
wujieli0207 <lwj0207@foxmail.com>
2021-12-08 21:07:55 +0800

    fix: 修正webform组件加载异常，增加datepicker

--- src/components/WebForm/index.vue
+++ src/components/WebForm/index.vue
@@ -12,17 +12,19 @@
           trigger: 'change',
         }"
       >
+        <!-- 输入框、富文本编辑 -->
         <template v-if="['input', 'textarea'].includes(item.type)">
           <el-input
             :type="item.type"
             v-bind="item.other"
-            v-model="form[item.value]"
             :placeholder="item.placeholder"
-          />
+            v-model="form[item.value]"
+          ></el-input>
         </template>
+        <!-- 下拉选择 -->
         <template v-if="item.type === 'select'">
           <el-select v-model="form[item.value]" placeholder v-bind="item.other">
-            <template v-for="op in item.options">
+            <template v-for="(op, index) in item.options" :key="index">
               <el-option
                 v-if="op && item.selectValue && item.selectLabel"
                 :key="op[item.selectValue]"
@@ -32,34 +34,40 @@
             </template>
           </el-select>
         </template>
+        <!-- 单选框 -->
         <template v-if="item.type === 'radio'">
           <el-radio-group v-model="form[item.value]" v-bind="item.other">
-            <template v-for="op in item.options">
+            <template v-for="(op, index) in item.options" :key="index">
               <el-radio
                 v-if="op && item.selectValue && item.selectLabel"
                 :key="op[item.selectValue]"
                 :label="op[item.selectLabel]"
                 :value="op[item.selectValue]"
-                >{{ op[item.selectValue] }}</el-radio
+                >{{ op[item.selectLabel] }}</el-radio
               >
             </template>
           </el-radio-group>
         </template>
+        <!-- 复选框 -->
         <template v-if="item.type === 'checkbox'">
           <el-checkbox-group v-model="form[item.value]" v-bind="item.other">
-            <template v-for="op in item.options">
+            <template v-for="(op, index) in item.options" :key="index">
               <el-checkbox
                 v-if="op && item.selectValue && item.selectLabel"
-                :key="op[item.selectValue]"
                 :label="op[item.selectLabel]"
+                v-model="op[item.selectValue]"
                 :value="op[item.selectValue]"
-                >{{ op[item.selectValue] }}</el-checkbox
+                >{{ op[item.selectLabel] }}</el-checkbox
               >
             </template>
           </el-checkbox-group>
         </template>
+        <!-- switch 选择 -->
         <template v-if="item.type === 'switch'">
           <el-switch v-model="form[item.value]" v-bind="item.other" />
+        </template>
+        <template v-if="item.type === 'date'">
+          <el-date-picker v-model="form[item.value]" v-bind="item.other"></el-date-picker>
         </template>
       </el-form-item>
     </el-form>
@@ -67,7 +75,7 @@
 </template>
 
 <script lang="ts">
-  import { defineComponent, PropType, computed } from "vue";
+  import { defineComponent, PropType, computed, watch } from "vue";
   import useState from "./hooks/useState";
   import type { IWebFormField } from "./type";
   export default defineComponent({
@@ -89,6 +97,17 @@
     setup: (props, context) => {
       const { webFormRef } = useState();
       const form = computed(() => props.modelValue);
+
+      watch(
+        () => form,
+        (val) => {
+          context.emit("update:modelValue", val);
+        },
+        {
+          deep: true,
+        }
+      );
+
       return {
         webFormRef,
         form,
--- src/components/WebForm/type.ts
+++ src/components/WebForm/type.ts
@@ -5,11 +5,11 @@ export interface IWebFormField {
   value: string | number; //  字段 value
   label: string; // 字段 标签
   // 字段录入项类型
-  type: "input" | "textarea" | "select" | "radio" | "checkbox" | "switch";
+  type: "input" | "textarea" | "select" | "radio" | "checkbox" | "switch" | "date";
   require: boolean; // 是否必录
   errMsg?: string; // 录入错误提示
   placeholder?: string;
-  options?: IBasicOption; // value-label
+  options?: IBasicOption[]; // value-label
   selectValue?: string | number;
   selectLabel?: string;
   other?: any; // 其他参数配置
